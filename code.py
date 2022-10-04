2f0bbcd983ff4b4d35175107637712172695c786
yared2022 <yared.asfaw@duke.edu>
2022-10-04 13:00:36 -0400

    adding code

---
+++ code.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+
+wdi = pd.read_csv(
+    "https://media.githubusercontent.com/media/nickeubank/MIDS_Data/master/World_Development_Indicators/wdi_small_tidy_2015.csv"
+)
+print(wdi.columns)
+
+wdi_infant_mortality = wdi.loc[
+    :,
+    [
+        "Country Name",
+        "GDP per capita (constant 2010 US$)",
+        "Mortality rate, infant (per 1,000 live births)",
+    ],
+]
